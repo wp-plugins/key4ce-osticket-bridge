d96014ac7b824b2f92b6759c54d1919b6fa8ac09
m.tiggelaar <m.tiggelaar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-01 15:42:39 +0000

    1.1 update
    git-svn-id: https://plugins.svn.wordpress.org/key4ce-osticket-bridge/trunk@941625 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ost-bridge.php
+++ ost-bridge.php
@@ -3,7 +3,7 @@
 Plugin Name: Key4ce osTicket Bridge
 Plugin URI: http://key4ce.com/osticket-bridge
 Description: Integrate osTicket (v1.8) or (v1.9) into wordpress. including user integration and scp
-Version: 1.1
+Version: 1.1.1
 Author: Key4ce
 Author URI: http://key4ce.eu
 License: GPLv3
