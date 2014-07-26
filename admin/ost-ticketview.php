71b814671ef1e10467db84a59aeacdcbce1984c3
m.tiggelaar <m.tiggelaar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-26 08:01:06 +0000

    Emergency 1.1.4 update due to 1.1.3 bugs
    git-svn-id: https://plugins.svn.wordpress.org/key4ce-osticket-bridge/tags/1.1.4@955164 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/ost-ticketview.php
@@ -0,0 +1,144 @@
+<?php
+require_once( WP_PLUGIN_DIR . '/key4ce-osticket-bridge/includes/functions.php' ); ?>
+<div class="wrap">
+<div class="headtitle">Reply to Support Request</div>
+<div style="clear: both"></div>
+<?php
+require_once( WP_PLUGIN_DIR . '/key4ce-osticket-bridge/admin/db-settings.php'); 
+require_once( WP_PLUGIN_DIR . '/key4ce-osticket-bridge/admin/header_nav_ticket.php'); 
+?>
+<div id="ticket_view">
+<div id="tic_number">Ticket ID #<?php echo $ticketinfo->number; ?></div>
+<div id="tic_icon"><a href="admin.php?page=ost-tickets&service=view&ticket=<?php echo $ticketinfo->number; ?>" title="Reload"><span class="Icon refresh"></span></a><span class="preply">&darr; <a href="#post">Post Reply</a></span></div>
+<div style="clear: both"></div>
+</div>
+<div id="tic_info_box">
+<table>
+<tr> 
+<td><b>Ticket Status:</b></td>
+<td><div>
+<?php
+	if($ticketinfo->status=='closed') {
+	echo '<font color=red>Closed</font>';
+	} 
+	elseif ($ticketinfo->status=='open' && $ticketinfo->isanswered=='0') {
+	echo '<font color=green>Open</font>';
+	}
+	elseif ($ticketinfo->status=='open' && $ticketinfo->isanswered=='1') {
+	echo '<font color=orange>Answered</font>';
+	}
+?>
+</div></td>
+<td><b>Username:</b></td>
+<td><div><?php echo $ticketinfo->name; ?></div></td>
+</tr>
+<tr> 
+<td><b>Department:</b></td>
+<td><div><?php echo $ticketinfo->dept_name; ?></div></td>
+<td><b>User Email:</b></td>
+<td><div><?php echo $ticketinfo->address; ?></div></td>
+</tr>
+<tr> 
+<td><b>Date Create:</b></td>
+<td><div><?php echo $ticketinfo->created; ?></div></td>
+<td><b>Priority:</b></td>
+<td> 
+<div><?php
+if($ticketinfo->priority_id=='4') {
+	echo '<div style="color: Red;"><strong>Emergency</strong></div>';
+	} 
+	elseif ($ticketinfo->priority_id=='3') {
+	echo '<div style="color: Orange;"><strong>High</strong></div>';
+	}
+	elseif ($ticketinfo->priority_id=='2') {
+	echo '<div style="color: Green;"><strong>Normal</strong></div>';
+	}
+	elseif ($ticketinfo->priority_id=='1') {
+	echo '<div style="color: Black;">Low</div>';
+	}
+    elseif ($ticketinfo->priority_id=='') {
+	echo '<div style="color: Black;">Normal</div>';
+	}
+ ?>
+</div>
+</td>
+</tr>
+</table>
+</div>
+<div style="clear: both"></div>
+<div id="tic_sub">
+<div id="tic_subject">Subject:</div>
+<div id="tic_subject_info"><?php echo $ticketinfo->subject; ?></div>
+<div style="clear: both"></div>
+</div>
+<div id="tic_thread_img_box">
+<div><span class="Icon thread">Ticket Thread</span></div>
+<div style="clear: both"></div>
+</div>
+<div id="ticketThread">
+<?php foreach($threadinfo as $thread_info) { ?>
+<table width="100%" style="width: 95%; margin: 10px 20px 10px 20px; border: 1px solid #aaa; border-bottom: 2px solid #aaa;" class="<?php echo $thread_info->thread_type; ?>">
+<tbody>
+<tr>
+<th><?php echo $thread_info->created; ?><span><?php echo $thread_info->poster; //if($hidename==1) { echo $thread_info->poster; } ?></span></th>
+</tr>
+<tr>
+<td><?php echo $thread_info->body;?></td>
+</tr>
+</tbody>
+</table>
+<?php } ?>
+<div style="clear: both"></div>
+</div>
+<div id="tic_post">
+<div id="tic_post_reply">Post a Reply</div>
+<div style="clear: both"></div>
+</div>
+<table align="center" width="95%" cellspacing="0" cellpadding="3" border="0">
+<tr>
+<td align="center"><a name="post"></a>
+<form name="ost-post-reply" id="ost-reply" action="admin.php?page=ost-tickets&service=view&ticket=<?php echo $ticketinfo->number; ?>" method="post" enctype="multipart/form-data">
+<input type="hidden" value="<?php echo $thread_info->ticket_id; ?>" name="tic_id">
+<input type="hidden" value="reply" name="a">
+<input type="hidden" name="usticketid" value="<?php echo $ticketinfo->number; ?>"/>
+<input type="hidden" name="usname" value="<?php echo $ticketinfo->name; ?>"/>
+<input type="hidden" name="usemail" value="<?php echo $ticketinfo->address; ?>"/>
+<input type="hidden" name="usdepartment" value="<?php echo $ticketinfo->dept_name; ?>"/>
+<input type="hidden" name="uscategories" value="<?php echo $ticketinfo->topic; ?>"/>
+<input type="hidden" name="ussubject" value="<?php echo $ticketinfo->subject; ?>"/>
+<input type="hidden" name="ustopicid" value="<?php echo $ticketinfo->topic_id; ?>"/>
+<input type="hidden" name="ademail" value="<?php echo $os_admin_email; ?>"/>
+<input type="hidden" name="adname" value="<?php echo $admin_fname; ?> <?php echo $admin_lname; ?>"/>
+<input type="hidden" name="stitle" value="<?php echo $title_name; ?>"/>
+<input type="hidden" name="sdirna" value="<?php echo $dirname; ?>"/>
+<input type="hidden" name="adreply" value="<?php echo $adminreply; ?>"/>
+<center><?php
+$content = '';
+$editor_id = 'message';
+$settings = array( 'media_buttons' => false );
+wp_editor( $content, $editor_id , $settings );?></center>
+</td>
+</tr>
+<tr>
+<td align="center">
+<?php
+	if($ticketinfo->status=='closed') {
+	echo '<center><label><input type="checkbox" name="open_ticket_status" id="open_ticket_status" value="open" checked>&nbsp;&nbsp;<font color=green>Reopen</font> Ticket On Reply</label></center>';
+	} elseif ($ticketinfo->status=='open') {
+	echo '<center><label><input type="checkbox" name="close_ticket_status" id="close_ticket_status" value="closed">&nbsp;&nbsp;<font color=red>Close</font> Ticket On Reply</label></center>';
+	}
+?>
+</td>
+</tr>
+<tr>
+<td align="center">
+<center>
+<input type="submit" name="ost-post-reply" value="Post Reply" class="button-primary" />&nbsp;&nbsp;
+<input type="button" value="Cancel - Go Back" class="button-primary" onClick="history.go(-1)"/>
+</center>
+</form>
+</td>
+</tr>
+</table>
+</div><!--End wrap-->
+<script language="javascript" src="<?php echo plugin_dir_url(__FILE__).'../js/fade.js';?>"></script>
---
+++ ost-bridge.php
@@ -0,0 +1,288 @@
+<?php
+/*
+Plugin Name: Key4ce osTicket Bridge
+Plugin URI: http://key4ce.com/osticket-bridge
+Description: Integrate osTicket (v1.8) or (v1.9) into wordpress. including user integration and scp
+Version: 1.1.4
+Author: Key4ce
+Author URI: http://key4ce.eu
+License: GPLv3
+
+Copyright (C) 2014  Key4ce
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/
+*/
+?>
+<?php
+add_action('admin_menu', 'mb_admin_menu');
+add_action('admin_head', 'mb_admin_css');
+
+register_activation_hook(__FILE__,'mb_install');
+register_uninstall_hook(__FILE__,'mb_uninstall');
+
+register_activation_hook(__FILE__,'mb_table_install');
+register_activation_hook(__FILE__,'mb_database_install');
+
+function mb_settings_link($actions, $file) {
+if(false !== strpos($file, 'ost-bridge'))
+    $actions['settings'] = '<a href="admin.php?page=ost-config">Config</a>';
+return $actions; 
+}
+add_filter('plugin_action_links', 'mb_settings_link', 2, 2);
+
+function addtemplate()
+{
+
+	require_once( WP_PLUGIN_DIR . '/key4ce-osticket-bridge/osticket-wp.php');
+}
+add_shortcode('addosticket', 'addtemplate');
+function custom_toolbar_openticket() {
+	global $wp_admin_bar;	
+	$wp_admin_bar->add_menu(array(
+		'id'     => 'opensupport',
+	        'title' => sprintf(__('Open Tickets')),			
+		'href' => get_admin_url(null,'admin.php?page=ost-tickets&service=list&status=open'),
+	));
+   
+}
+// Hook into the 'wp_before_admin_bar_render' action
+function custom_toolbar_supportticket() {
+	global $wp_admin_bar;	
+	$wp_admin_bar->add_menu(array(
+		'id'     => 'supportsupport',
+	        'title' => sprintf(__('Support Tickets')),			
+		'href' => get_admin_url(null,'admin.php?page=ost-tickets&service=list&status=all'),
+	));
+   
+}
+
+function mb_admin_menu() { 
+
+$config = get_option('os_ticket_config');
+extract($config);
+if (($database=="") || ($username=="") || ($password=="") || ($keyost_prefix=="")) {
+    $page_title = 'Support/Request List';
+    $menu_title = 'Tickets';
+} else {
+@$con = mysql_connect($host, $username, $password, true, 65536);
+if (mysqli_connect_errno()) {
+    $page_title = 'Support/Request List';
+    $menu_title = 'Tickets';
+} else {
+@mysql_select_db($database, $con);
+$result = mysql_query("SELECT number FROM ".@$keyost_prefix."ticket WHERE status='open' AND isanswered='0'");
+if (empty($result)) {
+	   $num_rows = '0';
+} else {
+$num_rows = mysql_num_rows($result);
+}
+    $page_title = 'Support/Request List';
+	if ($num_rows > 0) {
+    $menu_title = 'Tickets <span class="awaiting-mod"><span class="pending-count">' . $num_rows . '</span></span>';
+	} else {
+	$menu_title = 'Tickets'; } } }
+	$capability = 'manage_options';
+    $menu_slug = 'ost-tickets';
+    $function = 'ost_tickets_page';
+    $position = '51';
+	$icon_url = plugin_dir_url( __FILE__ ) . 'images/status.png';
+
+    add_menu_page($page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position);
+
+    $sub_menu_title = 'Email Tickets';
+    add_submenu_page($menu_slug, $page_title, $sub_menu_title, $capability, $menu_slug, $function);
+
+    $submenu_page_title = 'Settings';
+    $submenu_title = 'Settings';
+    $submenu_slug = 'ost-settings';
+    $submenu_function = 'ost_settings_page';
+    add_submenu_page($menu_slug, $submenu_page_title, $submenu_title, $capability, $submenu_slug, $submenu_function);
+	
+	$submenu_page_title = 'osT-Config';
+    $submenu_title = 'osT-Config';
+    $submenu_slug = 'ost-config';
+    $submenu_function = 'ost_config_page';
+    add_submenu_page($menu_slug, $submenu_page_title, $submenu_title, $capability, $submenu_slug, $submenu_function); 
+	
+	$submenu_page_title = 'Email Templates';
+    $submenu_title = 'Email Templates';
+    $submenu_slug = 'ost-emailtemp';
+    $submenu_function = 'ost_emailtemp_page';
+    add_submenu_page($menu_slug, $submenu_page_title, $submenu_title, $capability, $submenu_slug, $submenu_function);
+	
+	// Hook into the 'wp_before_admin_bar_render' action
+if (($database=="") || ($username=="") || ($password=="")) {
+    add_action( 'wp_before_admin_bar_render', 'custom_toolbar_supportticket', 999 );
+    } else {
+if(@$num_rows > 0)
+add_action( 'wp_before_admin_bar_render', 'custom_toolbar_openticket', 998 );
+else
+add_action( 'wp_before_admin_bar_render', 'custom_toolbar_supportticket', 999 );
+} }
+
+function mb_admin_css() {
+echo '<link rel="stylesheet" type="text/css" media="all" href="'.plugin_dir_url(__FILE__).'css/admin-style.css">';
+}
+
+function mb_install()
+{
+$host='localhost';
+$database='';
+$username='';
+$password='';
+$keyost_prefix='ost_';
+$supportpage='Support';
+
+$config=array('host'=>$host,'database'=>$database,'username'=>$username,'password'=>$password,'keyost_prefix'=>$keyost_prefix,'supportpage'=>$supportpage);
+update_option( 'os_ticket_config', $config);
+}
+
+function mb_table_install() {
+global $wpdb;
+$sql="";
+$table_name = $wpdb->prefix . "ost_emailtemp"; 
+ if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+    //table is not created. you may create the table here.
+    $sql ="DROP TABLE IF EXISTS ".$table_name.";\n";
+     $sql .= "CREATE TABLE $table_name (
+id mediumint(9) NOT NULL AUTO_INCREMENT,
+name varchar(32) NOT NULL,
+subject varchar(255) NOT NULL DEFAULT '',
+text text NOT NULL,
+created datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+updated datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+UNIQUE KEY id (id)
+)ENGINE=InnoDB  DEFAULT CHARSET=utf8";
+}   
+require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+dbDelta($sql);
+}
+
+function mb_database_install() {
+   global $wpdb;
+  
+   $table_name = $wpdb->prefix . "ost_emailtemp";
+    if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name)
+    {
+        $id = '1';
+   $name = "Admin-Response";
+   $subject = "Ticket ID [#\$ticketid]";
+   $text = "";
+   $rows_affected = $wpdb->insert( 
+   $table_name, 
+   array(
+   'id' => $id, 
+   'name' => $name,
+   'subject' => $subject,
+   'text' => $text,
+   'created' => current_time('mysql'), 
+   'updated' => current_time('mysql') 
+   ) );
+   $id = '2';
+   $name = "New-Ticket";
+   $subject = "Ticket ID [#\$ticketid]";
+   $text = "";
+   $rows_affected = $wpdb->insert( 
+   $table_name, 
+   array( 
+   'id' => $id,
+   'name' => $name,
+   'subject' => $subject,
+   'text' => $text,
+   'created' => current_time('mysql'), 
+   'updated' => current_time('mysql') 
+   ) ); 
+   $id = '3';
+   $name = "Post-Confirmation";
+   $subject = "Ticket ID [#\$ticketid]";
+   $text = "";
+   $rows_affected = $wpdb->insert( 
+   $table_name, 
+   array( 
+   'id' => $id,
+   'name' => $name,
+   'subject' => $subject,
+   'text' => $text,
+   'created' => current_time('mysql'), 
+   'updated' => current_time('mysql') 
+   ) ); 
+    }  
+}
+function mb_uninstall() 
+{
+    delete_option('os_ticket_config');
+    global $wpdb;
+    $table = $wpdb->prefix."ost_emailtemp";
+    $wpdb->query("DROP TABLE IF EXISTS $table");
+    $table_config = "ost_config";
+    $wpdb->query("DELETE FROM $table_config WHERE `namespace`='core' and `key`='smtp_username'");
+    $wpdb->query("DELETE FROM $table_config WHERE `namespace`='core' and `key`='smtp_password'"); 	
+    $wpdb->query("DELETE FROM $table_config WHERE `namespace`='core' and `key`='smtp_host'"); 		
+    $wpdb->query("DELETE FROM $table_config WHERE `namespace`='core' and `key`='smtp_port'"); 
+    $wpdb->query("DELETE FROM $table_config WHERE `namespace`='core' and `key`='smtp_status'"); 	
+}
+
+function ost_config_page() {
+    if (!current_user_can('manage_options'))
+    {
+      wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+    require_once( WP_PLUGIN_DIR . '/key4ce-osticket-bridge/admin/ost-config.php' );
+}
+
+function ost_settings_page() {
+$config = get_option('os_ticket_config');
+extract($config);
+    if (!current_user_can('manage_options'))
+    {
+      wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+    if (($database=="") || ($username=="") || ($password==""))
+    {
+    echo "<div class='headtitleerror'>osTicket Settings</div><div id='message' class='error'>" . __( '<p><b>Error:</b> You must complete "osTicket Data Configure" before this page will display... <a href="admin.php?page=ost-config">click here</a></p>', 'ost-menu' ) . "</div>";
+    } else {
+    require_once( WP_PLUGIN_DIR . '/key4ce-osticket-bridge/admin/ost-settings.php' );
+    }
+}
+
+function ost_emailtemp_page() {
+$config = get_option('os_ticket_config');
+extract($config);
+    if (!current_user_can('manage_options'))
+    {
+      wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+    if (($database=="") || ($username=="") || ($password==""))
+    {
+    echo "<div class='headtitleerror'>osTicket Email Templates</div><div id='message' class='error'>" . __( '<p><b>Error:</b> You must complete "osTicket Data Configure" before this page will display... <a href="admin.php?page=ost-config">click here</a></p>', 'ost-menu' ) . "</div>";
+    } else {
+    require_once( WP_PLUGIN_DIR . '/key4ce-osticket-bridge/admin/ost-emailtemp.php' );
+    }
+}
+
+function ost_tickets_page() {
+$config = get_option('os_ticket_config');
+extract($config);
+    if (!current_user_can('manage_options'))
+    {
+      wp_die( __('You do not have sufficient permissions to access this page.') );
+    }
+    if (($database=="") || ($username=="") || ($password==""))
+    {
+    echo "<div class='headtitleerror'>osTicket - Support/Request List</div><div id='message' class='error'>" . __( '<p><b>Error:</b> You must complete "osTicket Data Configure" before this page will display... <a href="admin.php?page=ost-config">click here</a></p>', 'ost-menu' ) . "</div>";
+    } else { 
+    if(isset($_REQUEST['service']) && $_REQUEST['service']=='view') 
+    { 
+    require_once( WP_PLUGIN_DIR . '/key4ce-osticket-bridge/admin/ost-ticketview.php' );
+    } else { 
+    require_once( WP_PLUGIN_DIR . '/key4ce-osticket-bridge/admin/ost-tickets.php' );
+    }
+  }
+}
+?>
